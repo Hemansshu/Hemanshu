c81f8af8a39d16eaaf1b66e249f16f5f37b87672
Hemansshu <mujk2108@gmail.com>
2024-11-04 20:22:33 +0530

    Add files via upload

---
+++ simple_app.py
@@ -0,0 +1,32 @@
+
+import streamlit as st
+import plotly.express as px
+import seaborn as sns
+import numpy as np
+
+tips = sns.load_dataset('tips')
+tips.head()
+
+
+fig1 = px.bar(tips, x="day", y="tip")
+
+fig2 = px.violin(tips, x="sex", y="tip")
+
+
+
+
+st.title("Data Visualization with Plotly")
+
+st.header("Plot 1: Bar Plot - Tips by Day")
+st.plotly_chart(fig1)
+st.markdown('''**Insight Observed**: (Add your insights here)''')
+
+
+
+
+st.header("Plot 2: Violin Plot - Tips by Gender")
+st.plotly_chart(fig2)
+st.markdown('''**Insight Observed**: (Add your insights here)''')
+
+
+
